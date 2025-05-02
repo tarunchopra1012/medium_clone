076f888c894cf3948aed145e3cff9f8fc1cf3675
tarun.chopra <tarun.chopra@inapp.com>
2025-05-02 14:53:22 +0530

    Changes the dashboard layout file

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -15,7 +15,7 @@ class PostController extends Controller
     {
         $posts = Post::orderBy("created_at","desc")->paginate(10);
         
-        return view("dashboard", [
+        return view("post.index", [
             'posts' => $posts,
         ]);
     }
--- resources/views/components/category-tabs.blade.php
+++ resources/views/components/category-tabs.blade.php
@@ -2,7 +2,6 @@
     <li class="me-2">
         <a href="#" class="inline-block px-4 py-2 text-white bg-blue-600 rounded-lg active" aria-current="page">All</a>
     </li>
-
     @forelse ($categories as $category)
         <li class="me-2">
             <a href="#"
